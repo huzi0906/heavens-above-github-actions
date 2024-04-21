f3f2396ceb1e4061382c2188ca86b0cf2fedc9fe
huzaifa <mhuzaifaahmad@gmail.com>
2024-04-21 15:30:08 +0500

    Update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    $: "readonly", // for jQuery
+  },
+  files: ["**/*.js"],
+  languageOptions: {
+    sourceType: "module",
+  },
+  extends: [pluginJs.configs.recommended],
+};
